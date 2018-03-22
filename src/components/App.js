afcc32b0d3a59009886da6d72cfe2dd9cae419c1
daria marczak <marczak.ria@gmail.com>
2018-03-22 10:25:11 +0100

    Finished app

--- src/components/App.js
+++ src/components/App.js
@@ -106,6 +106,7 @@ class App extends React.Component {
           updateFish={this.updateFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
+          storeId={this.props.match.params.storeId}
           deleteFish={this.deleteFish}
         />
       </div>
